48b7ff4a4c7ad0c649cd8de8102fc44a4eca401d
xujun <xvjun@haotang365.com.cn>
2018-04-20 11:43:11 +0800

    图片控件matisse

--- app/src/main/java/com/haotang/deving/mvp/view/activity/ImagePickerActivity.java
+++ app/src/main/java/com/haotang/deving/mvp/view/activity/ImagePickerActivity.java
@@ -1,9 +1,10 @@
 package com.haotang.deving.mvp.view.activity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.widget.DrawerLayout;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -12,105 +13,52 @@ import android.view.ViewGroup;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.haotang.deving.R;
 import com.haotang.deving.mvp.model.imageload.GlideImageLoader;
+import com.haotang.deving.mvp.view.activity.base.BaseActivity;
 import com.haotang.deving.mvp.view.adapter.TakePhotoImgAdapter;
-import com.jph.takephoto.app.TakePhoto;
-import com.jph.takephoto.app.TakePhotoActivity;
-import com.jph.takephoto.compress.CompressConfig;
-import com.jph.takephoto.model.CropOptions;
-import com.jph.takephoto.model.LubanOptions;
-import com.jph.takephoto.model.TImage;
-import com.jph.takephoto.model.TResult;
-import com.jph.takephoto.model.TakePhotoOptions;
 import com.ljy.devring.DevRing;
 import com.ljy.devring.other.RingLog;
-import com.ljy.devring.util.ColorBar;
 import com.youth.banner.Banner;
 import com.youth.banner.listener.OnBannerListener;
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.GlideEngine;
+import com.zhihu.matisse.engine.impl.PicassoEngine;
+import com.zhihu.matisse.filter.Filter;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindColor;
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
  * 相册操作
  */
-public class ImagePickerActivity extends TakePhotoActivity implements OnBannerListener {
+public class ImagePickerActivity extends BaseActivity implements OnBannerListener {
     protected final static String TAG = ImagePickerActivity.class.getSimpleName();
     @BindColor(R.color.colorPrimary)
     protected int mColor;
     @BindView(R.id.rv_take_photo)
     RecyclerView rvTakePhoto;
     Banner banner;
-    private TakePhoto takePhoto;
-    private Uri imageUri;
-    private boolean showProgressBar = true;//是否显示压缩进度
-    private boolean enableRawFile = true;//拍照压缩后是否保存原图
-    private boolean withOwnGallery = true;//是否使用TakePhoto自带相册
-    private boolean correctImage = true;//是否纠正拍照的照片旋转角度
-    private int limit = 1;//最多选择张数
-    private boolean iscrop = false;//是否纠正拍照的照片旋转角度
-    private int selectFrom = 2;//从哪选择：1从相册，2从文件
-    private boolean withWonCrop = true;//裁切工具：true:TakePhoto自带,false:第三方
-    private boolean rgCropSize = true;//尺寸/比例：true:宽/高,false:宽x高
-    private int width = 800;//压缩，裁剪的宽
-    private int height = 800;//压缩，裁剪的高
-    private int maxSize = 102400;//压缩完的大小不超多maxSize
     private TakePhotoImgAdapter takePhotoImgAdapter;
+    private static final int REQUEST_CODE_CHOOSE = 23;
+    private List<Uri> mDataList = new ArrayList<Uri>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getContentLayout() != 0) {
-            setContentView(getContentLayout());
-            ButterKnife.bind(this);
-        }
-        initBarColor();//初始化状态栏/导航栏颜色，需在设置了布局后再调用
-        initView(savedInstanceState);//由具体的activity实现，做视图相关的初始化
-        setView(savedInstanceState);//由具体的activity实现，做视图相关的设置
-        initData(savedInstanceState);//由具体的activity实现，做数据的初始化
-        initEvent();//由具体的activity实现，做事件监听的初始化
-    }
-
-    private void initBarColor() {
-        ViewGroup parent = (ViewGroup) findViewById(android.R.id.content);
-        if (parent.getChildAt(0) instanceof DrawerLayout) {
-            ColorBar.newDrawerBuilder()
-                    .applyNav(true)
-                    .navColor(mColor)
-                    .navDepth(0)
-                    .statusColor(mColor)
-                    .statusDepth(0)
-                    .build(this)
-                    .apply();
-        } else {
-            ColorBar.newColorBuilder()
-                    .applyNav(true)
-                    .navColor(mColor)
-                    .navDepth(0)
-                    .statusColor(mColor)
-                    .statusDepth(0)
-                    .build(this)
-                    .apply();
-        }
-    }
-
     protected int getContentLayout() {
         return R.layout.activity_take_photo;
     }
 
-    private ArrayList<TImage> mDataList = new ArrayList<TImage>();
-
+    @Override
     protected void initView(Bundle savedInstanceState) {
         DevRing.activityStackManager().pushOneActivity(this);
     }
 
+    @Override
     protected void setView(Bundle savedInstanceState) {
-        setTakePhoto();
         setAdapter();
         setBanner();
     }
@@ -138,53 +86,12 @@ public class ImagePickerActivity extends TakePhotoActivity implements OnBannerLi
         rvTakePhoto.setAdapter(takePhotoImgAdapter);
     }
 
-    private void setTakePhoto() {
-        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
-        if (!file.getParentFile().exists()) {
-            file.getParentFile().mkdirs();
-        }
-        imageUri = Uri.fromFile(file);
-        takePhoto = getTakePhoto();//获取实例
-        LubanOptions option = new LubanOptions.Builder()
-                .setMaxHeight(height)
-                .setMaxWidth(width)
-                .setMaxSize(maxSize)
-                .create();
-        CompressConfig config = CompressConfig.ofLuban(option);
-        config.enableReserveRaw(enableRawFile);//拍照压缩后是否保存原图
-        takePhoto.onEnableCompress(config, showProgressBar);//指定压缩工具,是否显示压缩进度
-
-        TakePhotoOptions.Builder builder = new TakePhotoOptions.Builder();
-        builder.setWithOwnGallery(withOwnGallery);//是否使用TakePhoto自带相册
-        builder.setCorrectImage(correctImage);//是否纠正拍照的照片旋转角度
-        takePhoto.setTakePhotoOptions(builder.create());
-    }
-
+    @Override
     protected void initData(Bundle savedInstanceState) {
     }
 
+    @Override
     protected void initEvent() {
-    }
-
-    @Override
-    public void takeSuccess(TResult result) {
-        if (result != null) {
-            ArrayList<TImage> images = result.getImages();
-            if (images != null && images.size() > 0) {
-                mDataList.addAll(images);
-                takePhotoImgAdapter.notifyDataSetChanged();
-            }
-        }
-    }
-
-    @Override
-    public void takeFail(TResult result, String msg) {
-        RingLog.e(TAG, "takeFail:" + msg);
-    }
-
-    @Override
-    public void takeCancel() {
-
     }
 
     @Override
@@ -193,52 +100,46 @@ public class ImagePickerActivity extends TakePhotoActivity implements OnBannerLi
         DevRing.activityStackManager().exitActivity(this); //退出activity
     }
 
-    @OnClick({R.id.btnPickByTake, R.id.btnPickBySelect})
+    @OnClick({R.id.zhihu, R.id.dracula})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.btnPickByTake:
-                if (iscrop) {//是否裁剪
-                    takePhoto.onPickFromCaptureWithCrop(imageUri, getCropOptions());
-                } else {
-                    takePhoto.onPickFromCapture(imageUri);
-                }
+            case R.id.zhihu:
+                Matisse.from(ImagePickerActivity.this)
+                        .choose(MimeType.allOf())
+                        .countable(true)
+                        .capture(true)
+                        .captureStrategy(
+                                new CaptureStrategy(true, "com.haotang.deving.mvp.model.fileprovider.MatisseFileProvider"))
+                        .maxSelectable(9)
+                        //.addFilter(new GifSizeFilter(320, 320, 5 * Filter.K * Filter.K))
+                        .gridExpectedSize(
+                                getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
+                        .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                        .thumbnailScale(0.85f)
+                        .imageEngine(new GlideEngine())
+                        //.setOnSelectedListener()
+                        .forResult(REQUEST_CODE_CHOOSE);
                 break;
-            case R.id.btnPickBySelect:
-                if (limit > 1) {//图片选择张数
-                    if (iscrop) {//是否裁剪
-                        takePhoto.onPickMultipleWithCrop(limit, getCropOptions());
-                    } else {
-                        takePhoto.onPickMultiple(limit);
-                    }
-                    return;
-                }
-                if (selectFrom == 2) {//从哪选择：1从相册，2从文件
-                    if (iscrop) {//是否裁剪
-                        takePhoto.onPickFromDocumentsWithCrop(imageUri, getCropOptions());
-                    } else {
-                        takePhoto.onPickFromDocuments();
-                    }
-                    return;
-                } else {
-                    if (iscrop) {//是否裁剪
-                        takePhoto.onPickFromGalleryWithCrop(imageUri, getCropOptions());
-                    } else {
-                        takePhoto.onPickFromGallery();
-                    }
-                }
+            case R.id.dracula:
+                Matisse.from(ImagePickerActivity.this)
+                        .choose(MimeType.allOf())
+                        .theme(R.style.Matisse_Dracula)
+                        .countable(false)
+                        .maxSelectable(9)
+                        .imageEngine(new GlideEngine())
+                        .forResult(REQUEST_CODE_CHOOSE);
                 break;
         }
     }
 
-    private CropOptions getCropOptions() {
-        CropOptions.Builder builder = new CropOptions.Builder();
-        if (rgCropSize) {
-            builder.setAspectX(width).setAspectY(height);
-        } else {
-            builder.setOutputX(width).setOutputY(height);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
+            mDataList.clear();
+            mDataList.addAll(Matisse.obtainResult(data));
+            takePhotoImgAdapter.notifyDataSetChanged();
         }
-        builder.setWithOwnCrop(withWonCrop);
-        return builder.create();
     }
 
     @Override
--- app/src/main/java/com/haotang/deving/mvp/view/adapter/TakePhotoImgAdapter.java
+++ app/src/main/java/com/haotang/deving/mvp/view/adapter/TakePhotoImgAdapter.java
@@ -1,16 +1,13 @@
 package com.haotang.deving.mvp.view.adapter;
 
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.haotang.deving.R;
-import com.haotang.deving.util.FileSizeUtil;
-import com.haotang.deving.util.StringUtil;
-import com.jph.takephoto.model.TImage;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -21,33 +18,17 @@ import java.util.List;
  * @author 徐俊
  * @date XJ on 2018/4/19 17:48
  */
-public class TakePhotoImgAdapter extends BaseQuickAdapter<TImage, BaseViewHolder> {
+public class TakePhotoImgAdapter extends BaseQuickAdapter<Uri, BaseViewHolder> {
     public TakePhotoImgAdapter(int layoutResId, List data) {
         super(layoutResId, data);
     }
 
     @Override
-    protected void convert(BaseViewHolder helper, TImage item) {
+    protected void convert(BaseViewHolder helper, Uri item) {
         ImageView imageView = helper.getView(R.id.iv_item_takephoto_imginfo);
         if (item != null) {
-            String compressPath = item.getCompressPath();
-            String originalPath = item.getOriginalPath();
-            String compressSize = "";
-            String originalSize = "";
-            if (StringUtil.isNotEmpty(compressPath)) {
-                compressSize = FileSizeUtil
-                        .formatFileSize(
-                                new File(compressPath)
-                                        .length(), false);
-                Glide.with(mContext).load(new File(compressPath)).into(imageView);
-            }
-            if (StringUtil.isNotEmpty(originalPath)) {
-                originalSize = FileSizeUtil
-                        .formatFileSize(
-                                new File(originalPath)
-                                        .length(), false);
-            }
-            helper.setText(R.id.tv_item_takephoto_imginfo, "原图大小为：" + originalSize + "压缩后大小为：" + compressSize);
+            Glide.with(mContext).load(item).into(imageView);
+            //helper.setText(R.id.tv_item_takephoto_imginfo, "原图大小为：" + originalSize + "压缩后大小为：" + compressSize);
         }
     }
 }
